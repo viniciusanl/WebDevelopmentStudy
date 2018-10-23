91a06210cfb8f33773ff4c188f5e9879cc6e5e10
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-10-23 11:51:16 -0300

    commit 23/10/2018

--- ExerciciosWeb/funcao/thisEBind.js
+++ ExerciciosWeb/funcao/thisEBind.js
@@ -9,3 +9,8 @@ pessoa.falar()
 const falar = pessoa.falar
 
 falar() // conflito entre paradgimas
+
+// bind = Metodo responsavel por amarrar determinado objeto
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
