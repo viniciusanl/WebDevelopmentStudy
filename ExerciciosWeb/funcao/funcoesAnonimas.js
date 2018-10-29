e3bf63f031ce4fb24b0cb80f158b608c025aa1f6
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-10-29 11:12:29 -0300

    commit 29/10/2018

---
+++ ExerciciosWeb/funcao/funcoesAnonimas.js
@@ -0,0 +1,27 @@
+const soma = function(x, y) {
+    return x + y
+}
+
+console.log(soma(1, 1))
+
+const imprimirResultado = function(a, b, operacao = soma) {
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3, 4)
+
+imprimirResultado(3, 4, soma)
+
+imprimirResultado(3, 4, function(x, y) {
+    return x - y
+})
+
+imprimirResultado(3, 4, (x, y) => x * y)
+
+const pessoa = {
+    falar: function() {
+        console.log('Opa')
+    }
+}
+
+pessoa.falar()
