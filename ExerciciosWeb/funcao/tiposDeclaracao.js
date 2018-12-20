eb7c7bcfbf5d2c9d4d57a322fbbf993f340d1164
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-12-20 19:10:39 -0200

    commit 20/12/2018

---
+++ ExerciciosWeb/funcao/tiposDeclaracao.js
@@ -0,0 +1,16 @@
+console.log(soma(10, 50))
+
+// Function Declaration
+function soma(x, y){
+    return x + y
+}
+
+// Function Express
+const sub = function (x, y) {
+    return x - y
+}
+
+// Named Function Express
+const mult = function mult(x, y){
+    return x * y
+}
